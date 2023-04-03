5c7ebe8dde72cc73bd7b622da2bdc77c3d6924f7
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-04-03 09:48:12 +0200

    AKSO: libseq init

---
+++ 2023/AKSO/libseq/trie_extra.c
@@ -0,0 +1,32 @@
+#include "trie_extra.h"
+#include "trie.h"
+
+/*
+    Biblioteka zawiera dodatkowe funkcje trie, które nie są używane w ostatecznym programie, ale są przydatne przy testowaniu.
+    Zakładamy, że użytkownik zaimplementował trie_value_write_dot()
+*/
+
+/*
+    Funkcja pomocnicza do trie_write_dot()
+    Wypisuje wierzchołek drzewa do formatu dot
+*/
+void trie_write_dot_node(FILE* fp, trie_node_t* node, trie_node_t* parent, int index) {
+    if (node == NULL) return;
+    if (parent != NULL)
+        fprintf(fp, "\t\"%p\" -> \"%p\" [label=\"%d\"]\n", parent, node, index);
+    fprintf(fp, "\t\"%p\" [label=\"%s\"]\n", node, node->name);
+    for (int i = 0; i < 3; i++) {
+        trie_write_dot_node(fp, node->children[i], node, i);
+    }
+}
+
+/*
+    Funkcja wypisuje drzewo do formatu dot
+*/
+void trie_write_dot(trie_root_t* root, FILE* fp) {
+    fprintf(fp, "digraph trie {\n");
+    fprintf(fp, "graph [ordering=\"out\"];\n");
+    fprintf(fp, "node [shape=record];\n");
+    trie_write_dot_node(fp, root->root, NULL, -1);
+    fprintf(fp, "}");
+}
---
+++ 2023/AKSO/libseq/trie_extra.h
@@ -0,0 +1,13 @@
+#ifndef trie_EXTRA_H
+#define trie_EXTRA_H
+
+#include "trie.h"
+#include <stdio.h>
+
+
+/* Implementowane przez użytkownika */
+void trie_extra_write_dot(const trie_extra_t* v, FILE* fp);
+
+void        trie_write_dot(trie_root_t* root, FILE* fp);
+
+#endif //trie_EXTRA_H
