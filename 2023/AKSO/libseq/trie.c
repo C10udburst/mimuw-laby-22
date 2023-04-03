284e00d6d6a1c756d2951d19186c9514f74cb137
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-04-03 10:27:37 +0200

    memory??

--- 2023/AKSO/libseq/trie.c
+++ 2023/AKSO/libseq/trie.c
@@ -88,6 +88,10 @@ int trie_insert_prefix(trie_root_t* root, const char* name) {
 
     bool added_node = false;
     trie_node_t* parent = root->root;
+    if (parent == NULL) { // TODO: wszędzie indziej takie coś
+        errno = ENOMEM;
+        return -1;
+    }
     for (size_t i = 0; i < n; i++) { // parent.name == name[0..(i-1)]
         if (parent->children[name[i]-'0'] != NULL) { // węzeł istnieje
             parent = parent->children[name[i]-'0'];
