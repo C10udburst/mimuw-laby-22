6d6765d3e3ac6247f48948f2ff8cbcbfc0039562
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-04-30 10:08:59 +0200

    sum.asm: 2/8 to dobry wynik

--- 2023/AKSO/sum/sum.asm
+++ 2023/AKSO/sum/sum.asm
@@ -4,28 +4,25 @@ global sum
 section .text
 
 sum:
-  xor r11, r11
-  xor rax, rax
-  mov r8, 1
-
+  ; jeśli n = 1 to nic nie trzeba robić
   cmp rsi, 1
   je .loop_end
   
-  add qword [rdi], 0 
+  xor rax, rax
+  mov r8, 1
+
+  ; ustawiamy r9 na wartość jaką należy wypełnić miejsce w x[1]
+  add qword [rdi], 0
+  js .if_neg_first ; r9 = SF ? 0xFFFFF : 0
+  xor r9, r9
+  jmp .loop_start
+.if_neg_first:
+  mov r9, 0xFFFFFFFFFFFFFFFF
 
 ; for (int r8 = 1; r8 < rsi; r8++)
 .loop_start:
   mov r10, qword [rdi + 8*r8]
-  js .if_neg_prev
-  mov qword [rdi + 8*r8], 0
-
-; [rdi + r8] = SF ? 0xFFF... : 0
-  jmp .endif_neg_ref
-.if_neg_prev:
-  mov qword [rdi + 8*r8], 0xFFFFFFFFFFFFFFFF
-.endif_neg_ref:
-
-  add [rdi+r8+1], r11
+  mov qword [rdi + 8*r8], r9
 
   mov rax, r8
   mul r8
@@ -38,14 +35,16 @@ sum:
 
   ; rax = rax/8
   shr rax, 3
-
-  ; TODO
-  xor r11, r11
-  shl r10, cl
-  adc r11, 0
-
   
   add qword [rdi+rax], r10
+
+  ; r9 = y > 0 ? 0x00.. : 0xFF..
+  js .if_neg
+  xor r9, r9
+  jmp .end_neg
+.if_neg:
+  mov r9, 0xFFFFFFFFFFFFFFFF
+.end_neg:
 
   inc r8
 
