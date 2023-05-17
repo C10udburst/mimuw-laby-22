95faf359e3c07f16f6fdc5b53b2976441a28ba94
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-05-17 08:53:08 +0200

    sum.asm: ten sam bug, ale mniej bajtów

--- 2023/AKSO/sum/sum.asm
+++ 2023/AKSO/sum/sum.asm
@@ -4,113 +4,79 @@ global sum
 %define x rdi
 %define i r8
 
-; [carry:current] = x[i] * 2^(i*i*64/n)
-%define current r9
-%define carry r10
-
-; r11 będzie trzymać od którego miejsca należy uzupełnić 0xffff... aby liczba x[] rzeczywiście trzymała wynik poprzednej operacji
-%define first_fff r11
-
-; rdx będzie trzymać do którego miejsca ma wrócić wywołanie .fill_with_fff
-; 0 - powrót do .finish_fill_1
-; -1 - powrót do .finish_fill_2
-; wpw - kontynuuj, czyli dodaj ostanią resztę
-%define return_mark rdx
+; [x1:x2:x3] = x[i] * 2^(i*i*64/n)
+%define x1 r9
+%define x2 r10
+%define y_len r11
 
 section .text
 
 sum:
-  or first_fff, -1        ; first_fff = INFINITY
-  xor i, i                ; i = 0
-  xor carry, carry        ; carry = 0
+  xor i, i
+  xor x1, x1
+  xor x2, x2
+  xor y_len, y_len
   xor rax, rax
 
-.loop_start:              ; for(int i=0; i<n; i++)
+  cmp n, 2
+  jb .skip
 
-  ; wczytuję current = x[i] oraz ustawiam x[i] = 0
-  xor current, current          ; current = 0
-  xchg current, qword [x + 8*i] ; x[i] = 0, current = x[i]
+.loop_start:    ; for(int i=0; i<n; i++)
+  xor rdx, rdx
+  xchg rdx, qword [x + 8*i]
 
-  ; fill_with_fff zapewnia, że w x[0...rax] rzeczywiście reprezentuje obecny yf
-  xor return_mark, return_mark  ; return_mark = 0, czyli .finish_fill_1
-  jmp .fill_with_fff            ; wywołanie funkcji fill_with_fff
-.finish_fill_1:
+  jmp .add_x
+.added_prev:
 
-  ; y += carry oraz first_fff = (SF) ? (rax + 1) : -1
-  or first_fff, -1 ; first_fff = - 1
-  add qword [x + 8*rax], carry
-  jns .y_positive
-  mov first_fff, rax  ; jeśli SF to first_fff = rax
-  inc first_fff
-.y_positive:
-
-.carry_empty:
+  mov x1, rdx
+  test x1, x1
+  js .neg_x1
+  xor x2, x2
+  jmp .pos_x1
+.neg_x1:
+  or x2, -1
+.pos_x1:
 
   mov rax, i    ; rax = i
   mul i         ; rax = i*i
   shl rax, 6    ; rax = 64*i*i
   div n         ; rax = 64*i*i / n
 
-  ; wyliczanie carry = (current < 0) ? -1 : 0
-  xor carry, carry            ; carry = 0
-  test current, current       ; SF = current < 0
-  jns .current_positive       ; SF > 0
-  or carry, -1                ; if (SF > 0) carry = -1
-.current_positive:
-
   ; Wyliczanie który blok z x[] wybrać i o ile pomnożyć current
   mov rcx, rax
   and rcx, 64 - 1    ; rcx = rax % 64, przesunięcie, które jest mniejsze niż indeks
   shr rax, 6         ; rax = rax / 64, wybór indeksu
 
-  ; mnożenie current przez 2^((64*i*i/n) % 64) i zapisywanie wyniku do [carry:current]
-  ; ponieważ cl < 64 (bo rcx = rax % 64), to wiemy na pewno że znak zostanie zachowanym,
-  ; bo zostanie przynajmniej 1 bit z pierwotnego carry
-  shld carry, current, cl       ; przesuwamy cl bitów z current do carry
-  shl current, cl               ; mnożymy current razy 2^cl
-
-  or return_mark, -1   ; return_mark = -1, czyli .finish_fill_2
-  jmp .fill_with_fff   ; wypełnienie -1
-.finish_fill_2:
-  
-  add qword [x + 8*rax], current ; y += current mod 64
-  adc carry, 0                   ; carry += CF
-  inc rax  ; rax trzyma adres poprzedniego dodawania, carry znajduje się o jeden indeks wyżej
+  shld x2, x1, cl
+  shl x1, cl  
 
   inc i
-
-  ; jeśli i<n to kontynuuj pętle
   cmp i, n
   jb .loop_start
+  xor n, n
 
-  ; ustaw return_mark na wartość inną niż 0 lub -1 
-  ; aby .fill_with_fff kontynuuowało, a wiemy, że 0<n<=2^29
-  mov return_mark, n
+.add_x:
+  inc rax
 
-; while(first_fff <= rax)
-;     x[first_fff++] = -1
-; jmp {
-;     .finish_fill_1 jeśli return_mark = 0
-;     .finish_fill_2 jeśli return_mark = -1
-;     nigdzie wpw
-; } to podejście zajmuje mniej miejsca niż lea [rel .finish_fill_1], itd
-.fill_with_fff:
-  cmp rax, first_fff
-  jb .end_fill_fff                ; first_fff > rax
-  or qword [x + 8*first_fff], -1  ; x[first_fff] = -1 = 0xfffffff...
-  inc first_fff                   ; first_fff++
-  jmp .fill_with_fff              ; first_fff <= rax
-.end_fill_fff:
-  test return_mark, return_mark
-  jz .finish_fill_1    ; return_mark = 0
-  js .finish_fill_2    ; return_mark = -1
-  ; jeśli tu jesteśmy do znaczy że .fill_with_fff wywołane na samym końcu
+.extend_y:
+  test qword [x + 8*y_len], -1
+  jns .y_positive
+.fill_y: ; ujemne y
+  cmp rax, y_len
+  jbe .extend_done
+  inc y_len
+  or qword[x + 8*y_len], -1
+  jmp .fill_y
+.y_positive:
+  cmp rax, y_len
+  cmova y_len, rax
+.extend_done:
+  add qword [x + 8*rax - 8], x1
+  adc qword [x + 8*rax], x2
+.add_end:
+  test n, n
+  jnz .added_prev
 
-; dodawanie ostaniego carry
-  cmp rax, n
-  jae .rax_too_big
-  add qword [x + 8*rax], carry
-
-.rax_too_big:
+.skip:
   ret
 
