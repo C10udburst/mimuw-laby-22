4ae6fe964d5939ae473a6cef530ae509248cc56f
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-05-18 18:27:22 +0200

    sum.asm: 164 -> 158

--- 2023/AKSO/sum/sum.asm
+++ 2023/AKSO/sum/sum.asm
@@ -1,10 +1,11 @@
 global sum
 
 %define n rsi
+%define n_32 esi
 %define x rdi
 %define i r8
 
-; [x1:x2:x3] = x[i] * 2^(i*i*64/n)
+; [x2:x1] = x[i] * 2^(i*i*64/n)
 %define x1 r9
 %define x2 r10
 %define y_len r11
@@ -18,9 +19,6 @@ sum:
   xor y_len, y_len
   xor rax, rax
 
-  cmp n, 2
-  jb .skip
-
 .loop_start:    ; for(int i=0; i<n; i++)
   xor rdx, rdx
   xchg rdx, qword [x + 8*i]
@@ -28,13 +26,11 @@ sum:
   jmp .add_x
 .added_prev:
 
-  ; ustalanie [x1:x2] = x[i]
-  mov x1, rdx   ; x1 = x[i]
-  xor x2, x2    ; x2 = (x1 < 0) ? -1 : 0
-  test x1, x1
-  jns .x1_positive
-  or x2, -1
-.x1_positive:
+  ; ustawia [x2:x1] = x[i]
+  mov rax, rdx  ; wpisuje x[i] do rax
+  cqo           ; rozszerza znak x[i] do rdx
+  mov x1, rax   ; ustala x1 na pierwszą cześć x[i]
+  mov x2, rdx   ; ustala x2 na pierwszą cześć x[i]
 
   mov rax, i    ; rax = i
   mul i         ; rax = i*i, rdx = 0
@@ -53,13 +49,13 @@ sum:
   inc i
   cmp i, n
   jb .loop_start
-  xor n, n
+  xor n_32, n_32
 
 .add_x:
   inc rax
 .extend_y:
-  test qword [x + 8*y_len], -1
-  jns .y_positive
+  cmp qword [x + 8*y_len], 0
+  jge .y_positive
 .fill_y:
   cmp rax, y_len
   jbe .extend_done
@@ -86,14 +82,12 @@ sum:
   inc rax
   cmp rax, y_len
   ja .ignore_carry
-  add [x + 8*rax], rcx
+  add qword [x + 8*rax], rcx
 
 .ignore_carry:
 
 .add_end:
-  test n, n
+  test n_32, n_32
   jnz .added_prev
 
-.skip:
   ret
-
