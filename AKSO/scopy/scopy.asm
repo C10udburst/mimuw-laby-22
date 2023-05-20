b0056bb8cde2a2fce0b57081d92986e8d4ccca7a
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-05-20 11:04:33 +0200

    scopy.asm: init

---
+++ AKSO/scopy/scopy.asm
@@ -0,0 +1,55 @@
+global _start
+
+; definicje syscall
+SYS_EXIT  equ 60
+SYS_OPEN  equ 2
+
+; rozmiary buforów
+READ_BUFFER equ 16
+WRITE_BUFFER equ 16
+
+; bits/fcntl-linux.h
+O_WRONLY equ 1o  ; tylko do zapisu
+O_CREAT equ 100o ; utwórz nowy
+O_EXCL equ 200o  ; błąd jeśli istnieje
+
+; uprawnienia nowo utworzonego pliku
+FMOD equ 664o ; rw-r--r--
+
+
+_start:
+  ; domyślna instalacja linuxa ustala ARG_MAX=2097152
+  ; więc teoretycznie qword jest tu niepotrzebny
+  cmp qword [rsp], 3
+  jnz .exit1
+  add rsp, 16  ; usuwamy ze stosu ilość argumentów i arg[0] (nazwę programu)
+  ; infile = [rsp], outfile = [rsp + 8]
+
+  ; otwieramy plik infile do czytania
+  mov eax, SYS_OPEN
+  mov rdi, [rsp]
+  xor esi, esi         ; esi = RDONLY = 0, tryb czytania
+  syscall
+  test eax, eax
+  js .exit1
+
+  ; otwieramy plik outfile do pisania
+  mov eax, SYS_OPEN
+  mov rdi, [rsp + 8]
+  mov esi, O_WRONLY | O_CREAT | O_EXCL ; utwórz plik to zapisywania
+  mov edx, FMOD ; ustaw uprawnienia pliku
+  syscall
+  test eax, eax
+  js .exit1
+
+
+  ; program zakończył się bezbłędnie
+  mov eax, SYS_EXIT
+  xor edi, edi  ; rdi = 0, $? = 0
+  syscall
+
+.exit1:
+  mov eax, SYS_EXIT
+  xor edi, edi  ; rdi = 0
+  inc edi       ; rdi = 1
+  syscall
